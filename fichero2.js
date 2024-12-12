2665c34f54e18202ddbc159a65e4d78789d171b4
Lidia Navarro <lidynaru@gmail.com>
2024-12-12 15:06:53 +0100

    actualizacion del contenido

--- fichero2.js
+++ fichero2.js
@@ -1,3 +1,3 @@
-console.log("****************");
-console.log("Soy el fichero II");
-console.log("****************");
+console.log("+++++++++++++++++");
+console.log("Soy el segundo fichero");
+console.log("+++++++++++++++++");
